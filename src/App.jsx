5dec0f1c7f5e36145323d685884306442b08907d
정문 <>
2023-01-16 14:16:24 +0000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React,{useState} from 'react';
+import './App.css';
+
+// To Do App 만들기
+// 1. 인풋창이 있고 버튼이 있다.
+// 2. 인풋창에 값을 입력하고 버튼을 클릭하면 아이템 추가 가능
+// 3. 아이템 삭제 버튼을 누르면 삭제가 가능하다.
+function App() {
+  return (
+    <main>
+      <input type="text">
+      <button>추가</button>
+    </main>
+  )
+}
+
+export default App;
