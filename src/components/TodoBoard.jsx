c6c2c9cc7820943e5fb73115139cf9b11a1b3f42
y55mun <69184668+y55mun@users.noreply.github.com>
2023-01-16 14:37:41 +0000

    add TodoItem.jsx

--- src/components/TodoBoard.jsx
+++ src/components/TodoBoard.jsx
@@ -1,10 +1,12 @@
 import React from "react"
+import TodoItem from "./TodoItem"
 
 function TodoBoard() {
 
   return (
     <div>
       <h1>Todo List</h1>
+      <TodoItem/>
     </div>
   )
 }
---
+++ src/components/TodoItem.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+
+function TodoItem() {
+
+  return(
+    <div>
+      공부하기
+    </div>
+  )
+  
+}
+
+export default TodoItem
